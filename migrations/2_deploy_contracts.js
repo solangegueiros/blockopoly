8b6fd6376cf5286730c81a7fbfd76e81ccfbf655
Solange Gueiros <solangegueiros@gmail.com>
2020-07-27 19:16:08 -0300

    03-truffle-assetmanager

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Bank = artifacts.require("Bank");
+var AssetManager = artifacts.require("AssetManager");
 
 module.exports = function(deployer) {
   deployer.deploy(Bank);
+  deployer.deploy(AssetManager);
 };
